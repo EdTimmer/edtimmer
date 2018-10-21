f0f1e411b9a49d5be7605a0ffea3e7028dcdb0d1
Edward Timmer <edtimmer@gmail.com>
2018-10-21 12:43:57 -0400

    formatting edits

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -10,45 +10,47 @@ import WilliamsPomona from './PortfolioWebsites/WilliamsPomona';
 import Sep from './PortfolioWebsites/Sep';
 
 class Portfolio extends Component {
-  componentDidMount() {
-    window.scrollTo(0, 0);
-  }
-  render() {
-    return (
-      <div>
-        <div className="bgimg-1">
-          <div>
-            <Nav header={'PORTFOLIO'} />
-          </div>
+    componentDidMount() {
+        window.scrollTo(0, 0);
+    }
+    render() {
+        return (
+            <div>
+                <div className="bgimg-1">
+                    <div>
+                        <Nav header={'PORTFOLIO'} />
+                    </div>
 
-          <Grid container spacing={24} justify="center" alignItems='center' style={{ backgroundColor: 'whitesmoke', padding: '0' }}>
-
-            <Grid item xs={4} container justify="center" alignItems='center'>
-              <PersonalWebsite />
-            </Grid>
-            <Grid item xs={4} container justify="center" alignItems='center'>
-              <PurlyLove />
-            </Grid>
-            <Grid item xs={4} container justify="center" alignItems='center'>
-              <SixWords />
-            </Grid>
-            <Grid item xs={4} container justify="center" alignItems='center'>
-              <UntappedTrivia />
-            </Grid>
-            <Grid item xs={4} container justify="center" alignItems='center'>
-              <Clickr />
-            </Grid>
-            <Grid item xs={4} container justify="center" alignItems='center'>
-              <WilliamsPomona />
-            </Grid>
-            <Grid item xs={4} container justify="center" alignItems='center'>
-              <Sep />
-            </Grid>
-          </Grid>
-        </div>
-      </div>
-    );
-  }
+                    <Grid container spacing={24} alignItems='center' style={{ backgroundColor: 'whitesmoke', padding: '0' }}>
+                        {/* justify="center" alignItems='center' */}
+                        <Grid item xs={4} container justify="center" alignItems='center'>
+                            <PersonalWebsite />
+                        </Grid>
+                        <Grid item xs={4} container justify="center" alignItems='center'>
+                            <span style={{ paddingTop: '0', marginTop: '0' }}>
+                                <PurlyLove />
+                            </span>
+                        </Grid>
+                        <Grid item xs={4} container justify="center" alignItems='center'>
+                            <SixWords />
+                        </Grid>
+                        <Grid item xs={4} container justify="center" alignItems='center'>
+                            <UntappedTrivia />
+                        </Grid>
+                        <Grid item xs={4} container justify="center" alignItems='center'>
+                            <Clickr />
+                        </Grid>
+                        <Grid item xs={4} container justify="center" alignItems='center'>
+                            <WilliamsPomona />
+                        </Grid>
+                        <Grid item xs={4} container justify="center" alignItems='center'>
+                            <Sep />
+                        </Grid>
+                    </Grid>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default Portfolio;
